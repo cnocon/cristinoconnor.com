68f49a34a6cf0ab3a4f9864a9c679577165cf206
Cristin O'Connor <her@cristin.io>
2024-07-19 15:30:35 -0600

    Update Resume data for skills and employment

--- src/data/appData.ts
+++ src/data/appData.ts
@@ -1,6 +1,6 @@
 import { IoLogoJavascript, IoLogoCss3 } from "react-icons/io"
 import { FaReact } from "react-icons/fa"
-import { MdMobileFriendly } from "react-icons/md"
+import { MdMobileFriendly, MdOutlineSettingsInputComponent, MdDataObject } from "react-icons/md"
 import { BsFillPersonCheckFill, BsGit } from "react-icons/bs"
 import { SiTypescript } from "react-icons/si"
 import { ReactNode } from "react"
@@ -34,7 +34,7 @@ const data = {
       platform: "ui.dev",
       Icon: TbFishHook,
       instructor: null,
-      completedDate: "In Progress",
+      completedDate: "04/15/2022",
       certificateUrl: null,
       description: "",//"Anim ipsum excepteur adipisicing aliquip nulla pariatur exercitation aute consequat commodo officia veniam aliquip culpa. Culpa nisi elit eu labore aliqua ex anim. Adipisicing qui ea esse velit anim in do commodo nulla elit id. Fugiat do do tempor reprehenderit.",
       imageUrl: "https://ui.dev/images/hook.svg",
@@ -90,14 +90,39 @@ const data = {
     ],
     technicalSkills: [
       {
-        label: "JavaScript",
+        label: "React",
+        percentage: 85,
+        icon: FaReact
+      },
+      {
+        label: "React Component Libraries",
+        percentage: 82,
+        icon: MdOutlineSettingsInputComponent
+      },
+      {
+        label: "Full Stack JavaScript",
+        percentage: 80,
+        icon: IoLogoJavascript
+      },
+      {
+        label: "Web Accessibility",
+        percentage: 80,
+        icon: BsFillPersonCheckFill
+      },
+      {
+        label: "TypeScript",
         percentage: 80,
         icon: IoLogoJavascript
       },
       {
         label: "SCSS, LESS, CSS",
-        percentage: 75,
+        percentage: 80,
         icon: IoLogoCss3
+      },
+      {
+        label: "Playwright, Jest, TestCafé, RTL",
+        percentage: 75,
+        icon: MdDataObject
       },
       {
         label: "Responsive Web Development",
@@ -105,35 +130,20 @@ const data = {
         icon: MdMobileFriendly
       },
       {
-        label: "Git",
+        label: "Git, GitHub, GitHub Actions",
         percentage: 70,
         icon: BsGit
       },
       {
-        label: "React",
-        percentage: 70,
-        icon: FaReact
-      },
-      {
-        label: "a11y",
-        percentage: 70,
-        icon: BsFillPersonCheckFill
-      },
-      {
         label: "TypeScript",
-        percentage: 65,
+        percentage: 70,
         icon: SiTypescript
       },
-      // {
-      //   label: "Ruby",
-      //   percentage: 40,
-      //   icon: DiRuby
-      // },
-      // {
-      //   label: "RTL & Jest",
-      //   percentage: 40,
-      //   icon: BiTestTube
-      // }
+      {
+        label: "Ruby, Ruby on Rails",
+        percentage: 60,
+        icon: MdDataObject
+      }
     ],
     softSkills: [ "Empathy to be an effective teammate", "Organizational abilities for managing projects independently", "Research skills to find the best solutions", "Enthusiasm and passion for front end work", "Positive attitude and willingness to handle a wide variety of responsibilities", "Effective communicator with people from varying technical backgrounds", "Dedicated to ongoing professional development", "5+ years of Agile experience", "12+ years of experience working on web applications in various capacities" ],
     workHistory: [ {
@@ -144,8 +154,8 @@ const data = {
       start: "08/01/2022",
       end: "Present",
       companyUrl: "https://citybase.com",
-      description: [ `Maintain and scale the front ends of several client facing applications related to making payments`, `Care for and grow a proprietary React component library, and integrate those components successfully into myriad contexts`, `Incrementally set up and convert JavaScript codebases to use TypeScript (front end and full stack)`, `Participate in design critiques (along with other engineers) to help facilitate cross-discipline knowledge transfer and early detection of potential gotchas in development` ],
-      technologies: [ "AdvancedJS", "React", "TypeScript", "Functional Programming", "a11y" ],
+      description: [ `Maintained and added new features to multiple single page web applications, utilizing technologies including JavaScript, Yarn, NPM, Webpack, Parcel, React, Redux, RxJS, TypeScript, SCSS, and GraphQL`, `Care for and grow a proprietary React component library, and integrate those components successfully into myriad contexts`, `Incrementally set up and convert JavaScript codebases to use TypeScript (front end and full stack)`, `Participate in design critiques (along with other engineers) to help facilitate cross-discipline knowledge transfer and early detection of potential gotchas in development`, `Upgraded our CI pipeline by incorporating Playwright tests to automatically run against the latest commit in a PR, leveraging Docker and GitHub Actions`, `Planned and executed accessibility overhauls of several front-end applications to bring them into compliance with WCAG 2.2 AA`      ],
+      technologies: [ "AdvancedJS", "React", "TypeScript", "Functional Programming", "a11y", "GraphQL", "GitHub Actions", "Storybook" ],
       honors: []
     },
     {
@@ -156,7 +166,7 @@ const data = {
       start: "01/01/2022",
       end: "06/01/2022",
       companyUrl: "https://nabis.com",
-      description: [ `Worked with product managers and graphic designers to overhaul the existing UI/UX with the goal of creating a single pristine user experience throughout the application ecosystem`, `Oversaw the introduction of a new design system to the codebase using Chakra UI's component library, extending Chakra UI where needed for custom components using React, GraphQL, CSS, and SCSS`, `Supervised two consultants in developing reusable components for Nabis' new design system` ],
+      description: [ `Worked with product managers and graphic designers to overhaul the existing UI/ UX with the goal of creating a single pristine user experience throughout the application ecosystem`, `Oversaw the introduction of a new design system to the codebase using Chakra UI's component library, extending Chakra UI where needed for custom components using React, GraphQL, CSS, and SCSS`, `Supervised two consultants in developing reusable components for Nabis' new design system`, `Improved accessibility and performance of an online marketplace search utility` ],
       technologies: [ "ES6", "React", "TypeScript", "ChakraUI", "SASS" ],
       honors: []
     },
@@ -168,7 +178,7 @@ const data = {
       start: "02/01/2021",
       end: "12/31/2021",
       companyUrl: "https://www.vivint.com/",
-      description: [ "Coded the front end for multiple JavaScript-based sales applications, leveraging technologies including Vue.js, Typescript, LESS, and Ember.js", "Spearheaded the creation of new documentation and code quality standards for front end development in sales", "Provided full stack on-call support for emergencies, leveraging logs and data from Splunk, Dynatrace, and AppDynamics to quickly identify and fix critical bugs under pressure (in a 24/7 rotation for a week each month)", "Created a new onboarding process for front end and full stack engineers in sales (for current employees and new hires) that aimed to reduce sales’ isolation from the company’s broader front end initiatives and practices" ],
+      description: [ `Coded the front-end for multiple JavaScript-based sales applications, leveraging technologies including Vue.js, TypeScript, LESS, and Ember.js`, `Spearheaded the creation of new documentation and code quality standards for front-end development within the sales department`, `Provided full stack on-call support for emergencies, leveraging logs and data from Splunk, Dynatrace, and AppDynamics to quickly identify and ﬁx critical bugs under pressure (in a 24/7 rotation for a week each month)`, `Created a new onboarding process for front-end and full stack engineers in sales (for current employees and new hires) that aimed to reduce sales' isolation from the company's broader front-end initiatives and practices`],
       technologies: [ "Ember", "Vue", "LESS", "TypeScript" ],
       honors: [ { name: "Innovation Week Presenter", detail: "Nov 2021" } ]
     },
@@ -180,7 +190,7 @@ const data = {
       start: "02/01/2020",
       end: "02/01/2021",
       companyUrl: "https://www.clearlink.com/",
-      description: [ "Used mockups to code responsive websites using Handlebars, SCSS, JavaScript, and a proprietary component library", "Assisted in maintaining and extending applications using JavaScript", "Mentored and supported junior developers", "Extended functionality of component library core package", "Supported a broad range of browsers and devices with a focus on accessibility" ],
+      description: [ `Coded responsive websites from mockups using Handlebars, SCSS, JavaScript, and a proprietary component library`, `Assisted in maintaining and extending applications using JavaScript`, `Mentored and supported junior developers`, `Extended functionality of component library core package`, `Supported a broad range of browsers and devices with a focus on accessibility`],
       technologies: [ "JavaScript", "PHP", "React", "Gatsby" ],
       honors: [ { name: "Frontend Project Showcase Winner", detail: "July 2020" }, { name: "Frontend Project Showcase Winner", detail: "Oct 2020" } ]
     },
@@ -192,7 +202,7 @@ const data = {
       start: "Apr 2018",
       end: "Jun 2019",
       companyUrl: "https://www.thesciongroup.com/",
-      description: [ "Increased average page load speed by a factor of four", "Updated in-house business intelligence software to use StimulusJS, DataTables, Highcharts, Rails 5, and data from multiple third party APIs including ADP, Entrata, Centro, and Chatmeter", "Redesigned UI/UX for a more modern, clean look and feel", "Aggregated over a dozen third party data sources into a data warehouse via an ETL application" ],
+      description: [ `Increased average page load speed by a factor of four`, `Updated in-house business intelligence software to use StimulusJS, DataTables, Highcharts, Rails 5, and data from multiple third party APIs including ADP, Entrata, Centro, and Chatmeter`, `Redesigned UI/UX for a more modern, clean, look and feel`, `Aggregated over a dozen third party data sources into a data warehouse via an ETL application` ],
       technologies: [ "Ruby", "Rails", "JavaScript", "SQL" ],
       honors: []
     }
